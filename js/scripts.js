bdce9fbb6a1a448cb5e4422705f8c1c3f38a0e23
Ruben Rhodes & Amy Vaillancourt-Sals <student@epicodus.com>
2014-07-22 14:22:11 -0700

    add click functions to increase levels

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,15 @@ var Tamagotchi = {
   },
   timePasses: function() {
     this.foodLevel -= 1;
+    this.sleepLevel -= 1;
+    this.activityLevel -= 1;
   },
   isAlive: function() {
     Tamagotchi.initialize();
-    if (this.foodLevel > 0) {
-      return true;
-    } else {
+    if ((this.foodLevel < 0) || (this.sleepLevel < 0) || (this.activityLevel < 0)) {
       return false;
+    } else {
+      return true;
     }
   },
 
@@ -24,15 +26,29 @@ $(document).ready(function(){
   $("#start").click(function(event) {
     var newTamagotchi = Object.create(Tamagotchi);
     newTamagotchi.initialize(prompt('Name your Tamagotchi!'));
-    newTamagotchi.timePasses();
-    $("#start").append("<p>" + newTamagotchi.foodLevel + "</p>");
 
-    // var count = 0;
-    // var time = window.setInterval(function(){
-    //   count += 1;
-    //   console.log(count);
-    // },1000)
+    var timer = window.setInterval(function(){
+      newTamagotchi.timePasses();
+      newTamagotchi.isAlive();
+      if (newTamagotchi.isAlive()) {
+        $("#food-level p").text(newTamagotchi.foodLevel);
+        $("#sleep-level p").text(newTamagotchi.sleepLevel);
+        $("#activity-level p").text(newTamagotchi.activityLevel);
+      } else {
+        $("#start").text("DEAD!");
+        clearInterval(timer);
+      }
+    }, 1000);
 
-
+    $("#food-level").click(function() {
+      newTamagotchi.foodLevel += 1
+    });
+    $("#sleep-level").click(function() {
+      newTamagotchi.sleepLevel += 1
+    });
+    $("#activity-level").click(function() {
+      newTamagotchi.activityLevel += 1
+    });
   });
+
 });
